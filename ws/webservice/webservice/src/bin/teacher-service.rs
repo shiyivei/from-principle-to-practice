782e0075e9b4ac3068c7f93e83668aca0d45aed3
shiyivei <83023417+jianquanqin@users.noreply.github.com>
2022-04-08 23:49:16 +0800

    Add files via upload

--- ws/webservice/webservice/src/bin/teacher-service.rs
+++ ws/webservice/webservice/src/bin/teacher-service.rs
@@ -8,6 +8,8 @@ mod handlers;
 mod routers;
 #[path = "../state.rs"]
 mod state;
+#[path = "../models.rs"]
+mod models;
 
 use routers::*;
 use state::AppState;
@@ -17,6 +19,7 @@ async fn main() -> io::Result<()> {
      let shared_data = web :: Data ::new(AppState{
           health_check_response: "I'm OK.".to_string(),
           visit_count: Mutex::new(0),
+          courses:Mutex::new(vec![])
 
      });
 
@@ -24,7 +27,8 @@ async fn main() -> io::Result<()> {
           App::new()
           .app_data(shared_data.clone())
           .configure(general_routes)
+          .configure(course_routes)
      };
 
-     HttpServer::new(app).bind("127.0.0.1:3030")?.run().await
+     HttpServer::new(app).bind("127.0.0.1:3000")?.run().await
 }
