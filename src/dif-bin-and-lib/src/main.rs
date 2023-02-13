4c48cd2b4f14a188b8fba72702e5b39b1b18acf8
shiyivei <shiyivei@outlook.com>
2023-02-13 18:10:50 +0800

    add review from-principle-to-practice.md

--- src/dif-bin-and-lib/src/main.rs
+++ src/dif-bin-and-lib/src/main.rs
@@ -1,34 +1,22 @@
-#[allow(unused)]
-fn main() {
-    unsafe {
-        let mut a = "hello".to_string();
-        let b = &mut a;
-        // let c = &mut a;
+use std::marker::PhantomData;
 
-        b;
-        // c;
-    }
+#[derive(Debug)]
+struct Container<T, U> {
+    data: T,
+    marker: PhantomData<U>,
+}
 
-    // 解引用静态变量
-    static mut COUNTER: u32 = 0;
-    let inc = 3;
-
-    unsafe {
-        COUNTER += inc;
-        println!("Counter: {}", COUNTER);
-    }
-
-    use std::future::Future;
-    // async 真正会返回 Future<Output = u8>, 而不是看上去的u8
-    async fn foo() -> u8 {
-        5
-    }
-
-    // async 块用法，返回 "impl Future<Output = u8>"
-    fn bar() -> impl Future<Output = u8> {
-        async {
-            let x = foo().await;
-            x + 5
+impl<T, U> Container<T, U> {
+    fn new(data: T) -> Container<T, U> {
+        Container {
+            data,
+            marker: PhantomData,
         }
     }
+}
+
+fn main() {
+    // 我们知道结构体的第二个字段是u32，但是它的值是多少我们并不在意
+    let _container: Container<i32, u32> = Container::new(42);
+    println!("{:?}", _container)
 }
