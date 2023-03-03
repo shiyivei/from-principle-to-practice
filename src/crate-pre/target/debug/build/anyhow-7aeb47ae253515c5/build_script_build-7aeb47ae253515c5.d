084afba6ab7f0fb7eb44e67aea6e5652151af41e
shiyivei <shiyivei@outlook.com>
2023-03-03 21:32:14 +0800

    add atomic type

--- src/crate-pre/target/debug/build/anyhow-7aeb47ae253515c5/build_script_build-7aeb47ae253515c5.d
+++
@@ -1,5 +0,0 @@
-/Users/qinjianquan/Career/rust/from-principle-to-practice/src/crate-pre/target/debug/build/anyhow-7aeb47ae253515c5/build_script_build-7aeb47ae253515c5: /Users/qinjianquan/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/anyhow-1.0.69/build.rs
-
-/Users/qinjianquan/Career/rust/from-principle-to-practice/src/crate-pre/target/debug/build/anyhow-7aeb47ae253515c5/build_script_build-7aeb47ae253515c5.d: /Users/qinjianquan/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/anyhow-1.0.69/build.rs
-
-/Users/qinjianquan/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/anyhow-1.0.69/build.rs:
--- src/crate-pre/target/debug/deps/tokio_macros-c5fa3cd3738d28c1.d
+++
@@ -1,7 +0,0 @@
-/Users/qinjianquan/Career/rust/from-principle-to-practice/src/crate-pre/target/debug/deps/libtokio_macros-c5fa3cd3738d28c1.dylib: /Users/qinjianquan/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/tokio-macros-1.8.2/src/lib.rs /Users/qinjianquan/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/tokio-macros-1.8.2/src/entry.rs /Users/qinjianquan/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/tokio-macros-1.8.2/src/select.rs
-
-/Users/qinjianquan/Career/rust/from-principle-to-practice/src/crate-pre/target/debug/deps/tokio_macros-c5fa3cd3738d28c1.d: /Users/qinjianquan/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/tokio-macros-1.8.2/src/lib.rs /Users/qinjianquan/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/tokio-macros-1.8.2/src/entry.rs /Users/qinjianquan/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/tokio-macros-1.8.2/src/select.rs
-
-/Users/qinjianquan/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/tokio-macros-1.8.2/src/lib.rs:
-/Users/qinjianquan/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/tokio-macros-1.8.2/src/entry.rs:
-/Users/qinjianquan/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/tokio-macros-1.8.2/src/select.rs:
